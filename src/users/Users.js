7d345f483b577046d526b0a6d9f135dbf55ebecc
saikumarjumbarthi <saikumar@umrk.co>
2019-03-06 16:46:01 +0530

    first commit

---
+++ src/users/Users.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import User from "./User";
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h3>These are the users of our application</h3>
+        <User age="20">Ajith</User>
+        <User age="21">Sujith</User>
+        <User age="22">Rajesh</User>
+      </div>
+    );
+  }
+}
+export default Users;
