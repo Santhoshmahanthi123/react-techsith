a83cc3c0fed9b96b411bba9a9256d5b3a002aaae
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-08 17:07:17 +0530

    Fragments added and works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 const Temp = () => {
-  return [<div>Hi......</div>, <div>Hello...</div>];
+  return (
+    <Fragment>
+      <div key="1">Hi......</div> <div key="2">Hello...</div>
+    </Fragment>
+  );
 };
 class App extends Component {
   render() {
