edbe3e38945b61463fa3017eb6b1089e6f9fc88c
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-08 15:33:52 +0530

    users changed

--- src/App.js
+++ src/App.js
@@ -32,6 +32,22 @@ class App extends Component {
     // replacing users inside the above state
     this.setState({ users: users });
   };
+  changeUserName = (id, e) => {
+    // based on index we need to find the users
+
+    const index = this.state.users.findIndex(user => {
+      return user.id === id;
+    });
+    // copies each index of individual user
+    const user = Object.assign({}, this.state.users[index]);
+    user.name = e.target.value;
+    const users = Object.assign([], this.state.users);
+    // changing the new user
+    users[index] = user;
+
+    // setting the state to users
+    this.setState({ users: users });
+  };
   render() {
     return (
       <div className="App">
@@ -42,6 +58,7 @@ class App extends Component {
                 age={user.age}
                 key={user.id}
                 deleteEvent={this.deleteUser.bind(this, index)}
+                changeEvent={this.changeUserName.bind(this, user.id)}
               >
                 {user.name}
               </User>
--- src/components/user.js
+++ src/components/user.js
@@ -5,6 +5,7 @@ const user = props => {
       <span>
         name : {props.children}, age: {props.age}
       </span>
+      <input onChange={props.changeEvent} value={props.children} />
       <button onClick={props.deleteEvent}>Delete</button>
     </li>
   );
