a1644956ed8875c14426f121a8cad41673031bae
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-08 17:59:42 +0530

    component life cycle with one child

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
+import Child from "./child";
 
 class App extends Component {
-  state = {
-    name: "raj"
-  };
-
   // constructor runs initially
   constructor() {
     super();
     this.state = {
       name: "santhosh"
     };
-
     // only runs one time when it initially renders
-    console.log("Initial render in constructor main");
+    console.log("constructor");
   }
+  componentWillMount() {
+    if (window.innerWidth < 500) {
+      this.setState({ innerWidth: window.innerWidth });
+    }
+    console.log("component will mount");
+  }
+
   render() {
-    return <div className="App">{this.state.name}</div>;
+    console.log("render");
+    return (
+      <div className="App">
+        <Child name={this.state.name} />
+        {this.state.name}
+        {this.state.innerWidth}
+      </div>
+    );
+  }
+  // children get rendered here
+  componentDidMount() {
+    console.log("component did mount");
   }
 }
 
---
+++ src/child.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Child extends Component {
+  // constructor runs initially
+  constructor() {
+    super();
+
+    // only runs one time when it initially renders
+    console.log("child constructor");
+  }
+  componentWillMount() {
+    console.log("child component will mount");
+  }
+  componentDidMount() {
+    console.log("child component did mount");
+  }
+  render() {
+    console.log("child render");
+    return <div className="App">name : {this.props.name}</div>;
+  }
+  // children get rendered here
+}
+
+export default Child;
