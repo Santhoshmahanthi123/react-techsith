8435739813a074785c5eab56dd1b357a81b7c73d
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-08 16:53:47 +0530

    react fragments

--- src/App.js
+++ src/App.js
@@ -1,70 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
-import User from "./components/user";
-import UniqueId from "react-html-id";
-
+const Temp = () => {
+  return (
+    <div>
+      <div>Hi......</div>
+      <div>Hello...</div>
+    </div>
+  );
+};
 class App extends Component {
-  constructor() {
-    super();
-    UniqueId.enableUniqueIds(this);
-    this.state = {
-      users: [
-        { id: this.nextUniqueId(), name: "santhosh", age: 22 },
-        {
-          id: this.nextUniqueId(),
-          name: "rajesh",
-          age: 23
-        },
-        {
-          id: this.nextUniqueId(),
-          name: "tinsy",
-          age: 24
-        }
-      ]
-    };
-    console.log(this.state);
-  }
-
-  // do delete a userwe have to copy those users in the below method
-  deleteUser = (index, e) => {
-    const users = Object.assign([], this.state.users);
-    users.splice(index, 1);
-    // replacing users inside the above state
-    this.setState({ users: users });
-  };
-  changeUserName = (id, e) => {
-    // based on index we need to find the users
-
-    const index = this.state.users.findIndex(user => {
-      return user.id === id;
-    });
-    // copies each index of individual user
-    const user = Object.assign({}, this.state.users[index]);
-    user.name = e.target.value;
-    const users = Object.assign([], this.state.users);
-    // changing the new user
-    users[index] = user;
-
-    // setting the state to users
-    this.setState({ users: users });
-  };
   render() {
     return (
       <div className="App">
-        <ul>
-          {this.state.users.map((user, index) => {
-            return (
-              <User
-                age={user.age}
-                key={user.id}
-                deleteEvent={this.deleteUser.bind(this, index)}
-                changeEvent={this.changeUserName.bind(this, user.id)}
-              >
-                {user.name}
-              </User>
-            );
-          })}
-        </ul>
+        <Temp />
       </div>
     );
   }
