ecff3f3ba9a46b0a7148f9e3baf4bbd087662a01
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-09 13:01:14 +0530

    React routing initial route

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 import "./App.css";
-const Temp = props => {
-  console.log("render Temp");
-  return <div>{props.val}</div>;
-};
-
-// pure component stops re-rendering for the same value
-class App extends PureComponent {
-  state = {
-    val: 1
-  };
-
-  // for every 2 seconds component updated that is it sets its state for ever 2 seconds
-  componentDidMount() {
-    setInterval(() => {
-      this.setState(() => {
-        return { val: 1 };
-      });
-    }, 2000);
-  }
-
-  // to fix above problem we have one method called should component update
-  // we use pure components now to update
-  // shouldComponentUpdate(nextProp, nextState) {
-  //   console.log("next state", nextState);
-  //   console.log("current state", this.state);
-  //   return this.state.val === nextState.val ? false : true;
-  // }
+import { BrowserRouter as Router } from "react-router-dom";
+import Route from "react-router-dom/Route";
+class App extends Component {
   render() {
     console.log("render app");
     return (
-      <div className="App">
-        <Temp val={this.state.val} />
-      </div>
+      <Router>
+        <div className="App">
+          <Route
+            path="/"
+            render={() => {
+              return <h1>Welcome to home page!</h1>;
+            }}
+          />
+        </div>
+      </Router>
     );
   }
 }
